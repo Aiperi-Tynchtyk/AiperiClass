724b744e77c283b9e50e9890855e989ee63ee559
Aiperi-Tynchtyk <tynchtyk.aiperi@gmail.com>
2023-10-13 11:09:50 -0500

    aiperimesage

--- src/test/java/AiperiClass.java
+++ src/test/java/AiperiClass.java
@@ -1,5 +1,18 @@
 public class AiperiClass {
     public static void main(String[] args) {
         System.out.println("hello guys");
+
+        // i want to use for loop
+        String word="apple";
+        String reverse="";
+        for (int i = word.length()-1; i >=0 ; i--) {
+            reverse+=word.charAt(i);
+        }
+        System.out.println(reverse);
+
+
     }
+
+
+
 }

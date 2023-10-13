15bfd2617fc67b4bba76f78a38560435d7c1544c
Nurlan Mukhambetov <nurlanm@Nurlans-MBP.hsd1.il.comcast.net>
2023-10-13 11:57:53 -0500

    added line and shenges

--- src/test/java/AiperiClass.java
+++ src/test/java/AiperiClass.java
@@ -4,6 +4,9 @@ public class AiperiClass {
     public static void main(String[] args) {
         System.out.println("hello guys");
         System.out.println("hello Aiperi I got you project");
+
+        // Aiperi Hello
+
     }
 
 
